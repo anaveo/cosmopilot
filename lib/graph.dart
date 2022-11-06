3d981751a579df937306889300969e50f4149ab6
anaveo <anaveolabs@gmail.com>
2022-11-06 02:10:22 -0500

    Code overhaul

--- lib/graph.dart
+++ lib/graph.dart
@@ -42,25 +42,50 @@ List<List<int>> terrain = [[5, 5, 5, 5, 5, 6, 6, 5, 6, 6, 6, 6, 5, 6, 7, 7, 7, 7
                            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 5, 6, 6, 5, 5, 6, 7, 7, 7, 7, 7, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 6, 6, 6, 6, 6, 6, 6]];
 
 Map<int, Map<int, int>> graph = {};
-void convertToGraph(List<List<int>> data){
+
+List<int> coordinateConv(int node) {
+  int x = node % 40;
+  int y = node ~/ 40;
+  return [x, y];
+}
+
+int weightEval(int node1, int node2) {
+  var n1c = coordinateConv(node1);
+  var n2c = coordinateConv(node2);
+  var weight = (terrain[n1c[0]][n1c[1]]
+      - terrain[n2c[0]][n2c[1]]).abs();
+  return weight;
+}
+
+void convertToGraph(List<List<int>> data) {
   int curKey = 0;
   // iterate over all rows of input data
   for (int i = 0; i < data.length; i++) {
     // iterate over all columns of input data
     for (int j = 0; j < data[0].length; j++) {
       graph[curKey] = {};            // initialize a K,V pair using current clk
+      int weight = 0;                // intermediate weight storage
+      int neighbor = 0;              // intermediate neighbor
       // establish neighbor connections
       if (i != data.length - 1) {    // ensure not at bottom edge of dataset
-        graph[curKey]?.addAll({curKey + data[0].length : 1}); // bottom neighbor
+        neighbor = curKey + data[0].length;
+        weight = weightEval(curKey, neighbor);
+        graph[curKey]?.addAll({neighbor : weight}); // bottom neighbor
       }
       if (i != 0) {             // ensure not at top edge of dataset
-        graph[curKey]?.addAll({curKey - data[0].length : 1}); // upper neighbor
+        neighbor = curKey - data[0].length;
+        weight = weightEval(curKey, neighbor);
+        graph[curKey]?.addAll({neighbor : weight}); // upper neighbor
       }
       if (j != data[0].length - 1) { // ensure not at right edge of dataset
-        graph[curKey]?.addAll({curKey + 1 : 1});              // right neighbor
+        neighbor = curKey + 1;
+        weight = weightEval(curKey, neighbor);
+        graph[curKey]?.addAll({neighbor : weight});              // right neighbor
       }
       if (j != 0) {             // ensure not at left edge of dataset
-        graph[curKey]?.addAll({curKey - 1 : 1});              // left neighbor
+        neighbor = curKey - 1;
+        weight = weightEval(curKey, neighbor);
+        graph[curKey]?.addAll({neighbor : weight});              // left neighbor
       }
       curKey++;
     }
